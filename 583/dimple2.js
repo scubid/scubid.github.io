466e3ea0cef8823125be5f1dd5d2a6b3e8d3fc19
scubid <47365906+scubid@users.noreply.github.com>
2019-03-26 15:29:07 -0600

    Create dimple2.js

---
+++ 583/dimple2.js
@@ -0,0 +1,13 @@
+var svg = dimple.newSvg("#chartContainer", 590, 400);
+d3.csv("netflixshowsdata.csv", function (data) {
+       var myChart = new dimple.chart(svg, data);
+       myChart.setBounds(95, 25, 475, 335)
+       myChart.addCategoryAxis("x", ["rating"]);
+       myChart.addCategoryAxis("y", "userRatingScore");
+       var z = myChart.addMeasureAxis("z", "userRatingScore");
+       var s = myChart.addSeries(["title","rating"], dimple.plot.bubble);
+       s.aggregate = dimple.aggregateMethod.max;
+       z.overrideMax = 200;
+       myChart.addLegend(240, 10, 330, 20, "right");
+       myChart.draw();
+});
